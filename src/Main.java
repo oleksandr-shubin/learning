969bb7588f8f5f24db691066ed1647f97933698c
Heruvim <oleksandr.shubin@gmail.com>
2016-11-08 23:46:45 +0200

    Threads created, using two ways

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,24 @@ import model.computer.Tablet;
 import model.store.Cart;
 
 public class Main {
+        public static class MyRunner implements Runnable {
+
+            @Override
+            public void run() {
+                System.out.println("Thread, which implements Runnable");
+            }
+        }
+
+        public static class MyThread extends Thread {
+
+            @Override
+            public void run(){
+                System.out.println("Thread, which extends Thread");
+            }
+        }
+
     public static void main(String[] args) {
-
-        Desktop desktop = new Desktop();
-        desktop.setRandomAccessMemory("Kingston 512 GB");
-        desktop.setVideoCard("NVIDIA GeForce 9600 GT");
-        desktop.setMotherboard("ASROCK");
-        desktop.setDiskDrive("SSD 256 GB");
-
-        Tablet tablet = new Tablet();
-        tablet.setGSMModule("ChinaConnection 3G/GSM");
-        tablet.setSensorType("Resistive");
-
-        Laptop laptop = new Laptop();
-        laptop.setKeyboard("Chicklet with backlight");
-        laptop.setTouchpad("ElanTech");
-
-        Cart<Computer> computersCart = new Cart<>();
-        computersCart.add(desktop);
-        computersCart.add(tablet);
-        computersCart.add(laptop);
-
-        computersCart.printConfigs();
+        new Thread(new MyRunner()).start();
+        new MyThread().start();
     }
 }
