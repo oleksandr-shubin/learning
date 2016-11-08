073b9141b6fdc16d7f0c95311f832bc2ba69bdcc
Heruvim <oleksandr.shubin@gmail.com>
2016-11-08 23:58:27 +0200

    MessageLoop created

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,7 @@
-import model.computer.Computer;
-import model.computer.Desktop;
-import model.computer.Laptop;
-import model.computer.Tablet;
-import model.store.Cart;
+import model.threads.MessageLoop;
 
 public class Main {
-        public static class MyRunner implements Runnable {
-
-            @Override
-            public void run() {
-                System.out.println("Thread, which implements Runnable");
-            }
-        }
-
-        public static class MyThread extends Thread {
-
-            @Override
-            public void run(){
-                System.out.println("Thread, which extends Thread");
-            }
-        }
-
     public static void main(String[] args) {
-        new Thread(new MyRunner()).start();
-        new MyThread().start();
+        new Thread(new MessageLoop()).start();
     }
 }
---
+++ src/model/threads/MessageLoop.java
@@ -0,0 +1,24 @@
+package model.threads;
+
+public class MessageLoop implements Runnable {
+    private final int MESSAGES_ARRAY_SIZE = 3;
+    private String[] messagesArray = new String[MESSAGES_ARRAY_SIZE];
+
+    {
+        messagesArray[0] = "first message";
+        messagesArray[1] = "second message";
+        messagesArray[2] = "third message";
+    }
+
+    @Override
+    public void run() {
+        for (String message : messagesArray) {
+            try {
+                Thread.sleep(4000);
+                System.out.println(message);
+            } catch (InterruptedException e) {
+                // logging of exception
+            }
+        }
+    }
+}
