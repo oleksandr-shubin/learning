b3a4128780bb104be35cb5540f6fb3255d7698a6
Heruvim <oleksandr.shubin@gmail.com>
2016-11-10 15:53:53 +0200

    SyncroSingleton created

---
+++ src/model/threads/SynchroSingleton.java
@@ -0,0 +1,18 @@
+package model.threads;
+
+public class SynchroSingleton {
+    private static SynchroSingleton instance;
+    private SynchroSingleton() {}
+
+    // double check implementation
+    public static SynchroSingleton getInstance() {
+        if (instance == null) {
+            synchronized (SynchroSingleton.class) {
+                if (instance == null) {
+                    instance = new SynchroSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
