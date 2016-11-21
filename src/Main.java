a820fc0b77d1ccbc5762e0b127ffd5741a2dee73
oleksandr-shubin <oleksandr.shubin@gmail.com>
2016-11-21 16:22:22 +0200

    SAX parser added to parse computerStore.xml

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,32 @@
-import model.validator.RegexValidator;
-import model.validator.StringValidator;
-import model.validator.Validator;
+import model.xml.ComputerHandler;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
-        String userName = "john_smith";
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            InputStream xmlInput = new FileInputStream("computerStore.xml");
+            SAXParser saxParser = factory.newSAXParser();
+            ComputerHandler handler = new ComputerHandler();
+            saxParser.parse(xmlInput, handler);
+            handler.displayResult();
 
-        String pattern = "\\w{4,16}";
-        Validator<String> regexValidator = new RegexValidator(pattern);
-        System.out.print("Regex model.validator: ");
-        if (regexValidator.isValid(userName)) {
-            System.out.println("valid");
-        } else {
-            System.out.println(regexValidator.getMessage());
-        }
-
-        Validator<String> stringValidator = new StringValidator(4, 16,
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                "abcdefghijklmnopqrstuvwxyz" +
-                "123456789_");
-        System.out.print("String model.validator: ");
-        if (stringValidator.isValid(userName)) {
-            System.out.println("valid");
-        } else {
-            System.out.println(stringValidator.getMessage());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
